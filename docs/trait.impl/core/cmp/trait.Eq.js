e7512e7e875a13ac7170a519212225b180c87d29
marcel <marcel44.gruszecki@gmail.com>
2026-06-04 22:14:16 +0900

    Init

---
+++ docs/trait.impl/core/cmp/trait.Eq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rtt_task_analyzer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"rtt_task_analyzer/app/ui/analysis_panel/enum.ALGO.html\" title=\"enum rtt_task_analyzer::app::ui::analysis_panel::ALGO\">ALGO</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"rtt_task_analyzer/app/ui/top_panel/enum.PANEL.html\" title=\"enum rtt_task_analyzer::app::ui::top_panel::PANEL\">PANEL</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"rtt_task_analyzer/communication/scheduler/enum.EventKind.html\" title=\"enum rtt_task_analyzer::communication::scheduler::EventKind\">EventKind</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[901]}
---
+++ docs/trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rtt_task_analyzer",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"rtt_task_analyzer/app/ui/analysis_panel/enum.ALGO.html\" title=\"enum rtt_task_analyzer::app::ui::analysis_panel::ALGO\">ALGO</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"rtt_task_analyzer/app/ui/top_panel/enum.PANEL.html\" title=\"enum rtt_task_analyzer::app::ui::top_panel::PANEL\">PANEL</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rtt_task_analyzer/app/struct.MyEguiApp.html\" title=\"struct rtt_task_analyzer::app::MyEguiApp\">MyEguiApp</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rtt_task_analyzer/app/ui/analysis_panel/struct.AnalysisPanel.html\" title=\"struct rtt_task_analyzer::app::ui::analysis_panel::AnalysisPanel\">AnalysisPanel</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1280]}
